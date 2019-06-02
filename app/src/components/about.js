ba4160731de69369bc23a421e7e64785556ea7d4
Thomas Claydon <tomclaydon102@gmail.com>
2019-06-02 19:40:26 -0400

    styling finished

--- app/src/components/about.js
+++ app/src/components/about.js
@@ -13,35 +13,54 @@ class About extends Component {
                 <p>
                   Professional Software Engineer and Full Stack Web Developer
                   who is passionate about cross technology integration. Prior
-                  experience in mechanical engineering (2+ years) working as a
-                  HVAC/Plumbing design engineer and (9+ years) in the
+                  experience in mechanical engineering <span>2+ years</span> working as a
+                  HVAC/Plumbing design engineer and <span>9+ years</span> in the
                   construction industry as Laborer, Drafter, and Manager.
                   Offering flexible technical and management skills suited for a
                   dynamic working environment.
                 </p>
               </div>
               <div className="container-skills">
-                <h3>Skills</h3>
+                {/* <h3>Skills</h3> */}
                 <div className="container-skills-lists">
-                  <ul>
-                    <li>HTML5</li>
-                    <li>CSS</li>
-                    <li>LESS</li>
-                    <li>Javascript</li>
-                    <li>React.js</li>
-                    <li>Redux.js</li>
-                  </ul>
-                  <ul>
-                    <li>Node.js</li>
-                    <li>Express</li>
-                    <li>SQLite</li>
-                    <li>knex.js</li>
-                    <li>PostgresSQL</li>
-                  </ul>
-                  <ul>
-                    <li>GitHub</li>
-                    <li>Python</li>
-                  </ul>
+                  <div className="container-skills-lists-sub">
+                    <h4>Languages</h4>
+                    <ul>
+                      <li>Javascript</li>
+                      <li>Python</li>
+                      <li>C</li>
+                    </ul>
+                  </div>
+                  <div className="container-skills-lists-sub">
+                    <h4>Front End</h4>
+                    <ul>
+                      <li>HTML5</li>
+                      <li>CSS</li>
+                      <li>LESS</li>
+                      <li>SASS</li>
+                      <li>React.js</li>
+                      <li>Redux.js</li>
+                    </ul>
+                  </div>
+                  <div className="container-skills-lists-sub">
+                    <h4>Back End</h4>
+                    <ul>
+                      <li>Node.js</li>
+                      <li>Express</li>
+                      <li>knex.js</li>
+                      <li>SQLite</li>
+                      <li>PostgresSQL</li>
+                      <li>JestJS.io</li>
+                    </ul>
+                  </div>
+                  <div className="container-skills-lists-sub">
+                    <h4>General Web</h4>
+                    <ul>
+                      <li>GitHub</li>
+                      <li>Netlify</li>
+                      <li>Heroku</li>
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
